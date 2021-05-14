b6d0fe143d3c5cb6078dbf135684423af185a5c5
e <3484766443@qq.com>
2021-05-14 10:51:09 +0800

    flex布局一点应用

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,39 +1,56 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
-    <div>{{count}}   {{fullName.fullname}}</div>
-    <button @click="addNum">点我加一</button><br>
+    <div>{{ count }} {{ fullName.fullname }}</div>
+    <button @click="addNum">点我加一</button><br />
     <router-link to="/about/child">Aboutchild</router-link>
     <todo-list></todo-list>
   </div>
+  <div class="buttonList">
+    <button>子按钮1</button>
+    <button>子按钮2</button>
+    <button>子按钮3</button>
+  </div>
   <router-view></router-view>
-  <render :level=1></render>
+  <render :level="1"></render>
 </template>
 <script>
-import Render from '@/views/Render'
-import { provide, reactive, readonly, ref } from 'vue'
-import TodoList from './TodoListDemo/TodoList.vue'
+import Render from "@/views/Render";
+import { provide, reactive, readonly, ref } from "vue";
+import TodoList from "./TodoListDemo/TodoList.vue";
 export default {
   setup() {
-    const count = ref('2')
+    const count = ref("2");
     const fullName = reactive({
-      'fullname':'姚泽宇'
-    })
-    provide('count',readonly(count))
-    provide('fullName',readonly(fullName))
-    return{
+      fullname: "姚泽宇",
+    });
+    provide("count", readonly(count));
+    provide("fullName", readonly(fullName));
+    return {
       count,
-      fullName //在这里返回的东西可以在组件其他任何地方使用
-    }
+      fullName, //在这里返回的东西可以在组件其他任何地方使用
+    };
   },
-  methods:{
-    addNum(){
-      this.count++
-    }
+  methods: {
+    addNum() {
+      this.count++;
+    },
   },
-  components:{
+  components: {
     Render,
-    TodoList
-  }
-}
+    TodoList,
+  },
+};
 </script>
+
+<style scoped>
+.buttonList{
+  height: 200px;
+  width: 400px;
+  margin: 0 auto;
+  display: flex;
+  background:blue;
+  justify-content: space-around;
+  align-items: center;
+}
+</style>
