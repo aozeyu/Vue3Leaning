a4bf2916794fb7663fb65b9557c6a4a2fd64f380
e <3484766443@qq.com>
2021-05-16 10:37:01 +0800

    使用mapstate简写计算属性

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,11 +2,12 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" @my-event="doSomeing"
-      >我是插槽内容<br>{{this.fatherMsg}}</HelloWorld>
+      >我是插槽内容<br />{{ this.fatherMsg }}</HelloWorld
+    >
     <router-link to="/homechild">Homechild</router-link>
-    <br>
+    <br />
     <button @click="addCount2">点我触发mutation</button>
-    <h4>{{this.count}}</h4>
+    <h4>{{ this.count }}</h4>
   </div>
   <router-view></router-view>
 </template>
@@ -14,15 +15,15 @@
 <script>
 // @ is an alias to /src
 import HelloWorld from "./HelloWorld.vue";
-
+import { mapState } from "vuex";
 export default {
   name: "Home",
-data() {
-  return {
-    fatherMsg:'我是与插槽同作用域的数据',
-    count:this.$store.state.count
-  }
-},
+  data() {
+    return {
+      fatherMsg: "我是与插槽同作用域的数据",
+    };
+  },
+  computed: mapState(["count"]), //mapState是一个函数 使得this.count = this.$store.state.count
   components: {
     HelloWorld,
   },
@@ -30,10 +31,10 @@ data() {
     doSomeing() {
       console.log("我是父组件传给你的自定义事件");
     },
-    addCount2(){
-      this.$store.commit('addCount')
+    addCount2() {
+      this.$store.commit("addCount");
       console.log(this.$store.state.count);
-    }
+    },
   },
 };
 </script>
